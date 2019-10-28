bb54e6bae58312f6d40676912ba7879df4d51bf1
agizzi <alexandra.n.gizzi@gmail.com>
2019-10-28 12:37:59 -0400

    start project

---
+++ config/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for confit project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'confit.settings')
+
+application = get_wsgi_application()
