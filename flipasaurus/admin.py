a83a2ceba386f59e51fc6d6095d98fb795b9e319
agizzi <alexandra.n.gizzi@gmail.com>
2019-10-28 13:44:38 -0400

    adds gitignore

--- flipasaurus/admin.py
+++ flipasaurus/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Card, Deck
 
 # Register your models here.
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Card)
+admin.site.register(Deck)
