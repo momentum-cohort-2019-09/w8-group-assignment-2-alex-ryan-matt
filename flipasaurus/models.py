465ba3724ddab3f7831d03759c90b12c5326603e
agizzi <alexandra.n.gizzi@gmail.com>
2019-10-28 13:20:27 -0400

    setting up models

---
+++ flipasaurus/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class User(AbstractUser):
+
+
+class Card(models.Model):
+  subject = models.CharField(max_length = 100, blank=True, null=True)
+  prompt = models.TextField()
+  description = models.TextField()
+  owner = models.ForeignKey(to="User", related_name="card", on_delete="models.CASCADE", ) #Make sure is read only .. FIX ME
+  correct_flips = models.IntegerField(default=0)
+  incorrect_flips = models.IntegerField(default=0)
+  success_rate = models.DecimalField(default=null, max_digits=5, decimal_places=2, blank=True, null=True)
+
+class Deck(models.Model):
+  title = models.CharField(max_length=100)
+  owner = models.ForeignKey(to="User", related_name="deck", on_delete="models.CASCADE",)
+  cards = models.ManyToManyField(to="Card", related_name="cards",)
+  
+
+
+
+
+
