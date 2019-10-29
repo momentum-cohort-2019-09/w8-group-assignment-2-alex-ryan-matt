b4f6c5447eabb04450820db1589e35eba46099b9
agizzi <alexandra.n.gizzi@gmail.com>
2019-10-29 13:22:03 -0400

    adjust model

---
+++ flipasaurus/serializers.py
@@ -0,0 +1,21 @@
+from flipasaurus.models import User, Card, Deck
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+  decks =  serializers.PrimaryKeyRelatedField(many=True, queryset=Deck.objects.all())
+
+  class Meta:
+    model = User
+    fields = ['id','username','decks']
+
+class CardSerializer(serializers.HyperlinkedModelSerializer):
+  class Meta:
+    model = Card
+    fields = ['subject', 'prompt', 'description','owner','correct_flips','incorrect_flips','success_rate']
+
+class DeckSerializer(serializers.HyperlinkedModelSerializer):
+  class Meta:
+    model = Deck
+    fields = ['title','owner','cards']
+
