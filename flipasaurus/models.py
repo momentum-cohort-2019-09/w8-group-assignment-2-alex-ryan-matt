998547cc4d73e3ca5bbf3ea2cd079b9832a9e9e7
agizzi <alexandra.n.gizzi@gmail.com>
2019-10-30 14:25:03 -0400

    working with api

--- flipasaurus/models.py
+++ flipasaurus/models.py
@@ -15,6 +15,7 @@ class Card(models.Model):
   correct_flips = models.IntegerField(default=0)
   incorrect_flips = models.IntegerField(default=0)
   success_rate = models.DecimalField(default=None, max_digits=5, decimal_places=2, blank=True, null=True)
+  deck = models.ForeignKey(to="Deck", related_name="cards", on_delete="models.CASCADE")
 
   def __str__(self):
     return self.prompt
@@ -23,7 +24,6 @@ class Deck(models.Model):
   subject = models.CharField(max_length = 100, blank=True, null=True)
   title = models.CharField(max_length=100)
   owner = models.ForeignKey(to="User", related_name="decks", on_delete="models.CASCADE",)
-  cards = models.ManyToManyField(to="Card", related_name="cards",)
   public = models.BooleanField(default=True) #HYPE
   updated_at = models.DateTimeField(default=timezone.now)
 
