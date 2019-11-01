b3e35a686e4cc50ceeee668f044e2a79e82b21e8
Matt Gouchoe-Hanas <mattgh21@gmail.com>
2019-11-01 18:00:23 -0400

    you may quiz yourselfy

--- flipasaurus/static/quiz.js
+++ flipasaurus/static/quiz.js
@@ -1,41 +1,48 @@
 let deck = JSON.parse(document.getElementById('data').textContent)
 let cards = []
+let current_card_index = 0
 
 
 let cardsCorrect = [];
 let cardsIncorrect = [];
 
+
+
 function flipCard(deck) {
     console.log(deck)
     let card = document.getElementById('card');
-    if (card.innerHTML === deck.cards[0].prompt) {
-        card.innerHTML = deck.cards[0].description
+    if (card.innerHTML === deck.cards[current_card_index].prompt) {
+        card.innerHTML = deck.cards[current_card_index].description
     } else {
-        card.innerHTML = deck.cards[0].prompt;
+        card.innerHTML = deck.cards[current_card_index].prompt;
     }
     document.getElementById('button').classList.toggle("hidden")
-    correctCard();
-    incorrectCard();
+
 }
 
 function correctCard() {
-    document.getElementById('correct').addEventListener('click', function(event) {
-        event.preventDefault()
-
-        deck.cards[0].correct_flips += 1
-        cardsCorrect.push(deck.cards[0])
-        console.log(cardsCorrect, 'correct')
-    })
+    deck.cards[current_card_index].correct_flips += 1
+    cardsCorrect.push(deck.cards[current_card_index])
+    current_card_index += 1;
+    flipCard(deck)
+    console.log(cardsCorrect, 'correct')
 }
 
 function incorrectCard() {
-    document.getElementById('incorrect').addEventListener('click', function(event) {
-        event.preventDefault()
-
-        deck.cards[0].incorrect_flips += 1
-        cardsIncorrect.push(deck.cards[0])
-        console.log(cardsIncorrect, 'incorrect')
-    })
+    deck.cards[current_card_index].incorrect_flips += 1
+    cardsIncorrect.push(deck.cards[current_card_index])
+    current_card_index += 1;
+    flipCard(deck)
+    console.log(cardsIncorrect, 'incorrect')
 }
 
-document.getElementById('card').innerHTML = deck.cards[0].prompt
+document.getElementById('correct').addEventListener('click', function(event) {
+    event.preventDefault()
+    correctCard()
+})
+document.getElementById('incorrect').addEventListener('click', function(event) {
+    event.preventDefault()
+    incorrectCard()
+})
+
+document.getElementById('card').innerHTML = deck.cards[current_card_index].prompt
