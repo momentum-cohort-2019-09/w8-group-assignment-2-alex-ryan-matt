46e394de17185650286db9ace9d1572c927b0714
agizzi <alexandra.n.gizzi@gmail.com>
2019-11-01 14:58:14 -0400

    lastest broken thing

--- flipasaurus/static/mr-scripty.js
+++ flipasaurus/static/mr-scripty.js
@@ -1,16 +1,16 @@
-// let prompt = document.querySelector('#id_prompt').value
-// let description = document.querySelector('#id_description').value
+let prompt = document.querySelector('#id_prompt').value
+let description = document.querySelector('#id_description').value
 
 
 console.log('i am working')
 
 
-document.querySelector('#actual_thing_we_want').addEventListener('submit', function (event) {
+document.getElementById('actual_thing_we_want').addEventListener('submit', function (event) {
   event.preventDefault()
   console.log('clicked')
-  fetch('/api/1/artisanal_card_create', {
+  fetch('/api/deck/{{pk}}/artisanal_card_create', {
     method: "POST",
-    body: JSON.stringify({ prompt: 'strang', description: 'iddescription', })
+    body: JSON.stringify({ prompt: prompt, description: description })
   })
     .then(response => { response.json() })
     .then(response => { console.log(response) })
